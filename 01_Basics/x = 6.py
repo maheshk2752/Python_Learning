6bd4a73273df79df7f42243c6b4e7ca158b04b53
Mahesh <beekhanimahesh@gmail.com>
2026-06-07 02:18:47 -0700

    some changed

--- 01_Basics/x = 6.py
+++ 01_Basics/x = 6.py
@@ -1,4 +1,10 @@
- a = 56
- b = 78
- sum = a + b
- print(sum)
+str1 = "apna"
+len1 = len(str1)
+print(len1)  
+str2 = "collage"
+len2 = len(str2)
+print(len2)
+
+final_str = str1 + " " +str2
+print(final_str)
+print(len(final_str))
