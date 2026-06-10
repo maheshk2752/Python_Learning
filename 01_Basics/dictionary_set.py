7dd81eb7586ecb4bf2e59cd965f3964155077227
Mahesh <beekhanimahesh@gmail.com>
2026-06-10 03:43:40 -0700

    dictionary

---
+++ 01_Basics/dictionary_set.py
@@ -0,0 +1,10 @@
+info = {
+    "name" : "apna college",
+    "subjects" : ["python", "C", "java"],
+    "topics" : ("dict", "set"),
+    "age" : 35,
+    "is adult" :true,
+    12.99 : 4.7
+}
+info["name"] = "mahesh"
+print(info)
--- 01_Basics/palindrome_list.py
+++ 01_Basics/palindrome_list.py
@@ -1,18 +1,3 @@
-# Create a list of numbers
-my_list = [1, 2, 1]
-
-# Create a copy of the original list
-copy_list = my_list.copy()
-
-new_list =[3,4,5,6]
-
-# Reverse the copied list
-copy_list.reverse()
-
-# Compare the original list with the reversed list
-if copy_list == new_list:
-    # If both lists are the same, it is a palindrome
-    print("Palindrome")
-else:
-    # If the lists are different, it is not a palindrome
-    print("Not Palindrome")
+grade = ["A", "B", "A", "A", "D"]
+grade.sort()
+print(grade)
